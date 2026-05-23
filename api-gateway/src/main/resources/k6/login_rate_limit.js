4351000514061a0fa3c456a4ebe832d1628d5610
NguyenDucguyen <nguyenhaibatrung05@gmail.com>
2026-05-23 12:07:04 +0700

    feat : add k6 complete

--- api-gateway/src/main/resources/k6/login_rate_limit.js
+++ api-gateway/src/main/resources/k6/login_rate_limit.js
@@ -14,19 +14,19 @@ export const options = {
             preAllocatedVUs: 20,
             maxVUs: 100,
             stages: [
-                { target: 10, duration: '10s' }, // Giai đoạn 1: Login chậm
-                { target: 50, duration: '20s' }, // Giai đoạn 2: Spam login liên tục
-                { target: 5, duration: '10s' },  // Giai đoạn 3: Giảm dần
+                { target: 10, duration: '10s' },
+                { target: 50, duration: '20s' },
+                { target: 5, duration: '10s' },
             ],
         },
     },
 };
 
 export default function () {
-    // 1. Target URL: Phải là địa chỉ của API Gateway (Port 8888 của bạn)
+
     const url = 'http://localhost:8888/api/v1/auth/login';
 
-    // 2. Payload: Giả lập gửi user/pass liên tục
+
     const payload = JSON.stringify({
         email: "nguyen@gmail.com",
         password: "123456"
@@ -35,20 +35,20 @@ export default function () {
     const params = {
         headers: {
             'Content-Type': 'application/json',
-            // KHÔNG gửi Authorization header (vì đang đi login mà)
+
         },
     };
 
-    // 3. Gửi POST Request (Login là POST)
+
     const res = http.post(url, payload, params);
 
-    // 4. Kiểm tra
+
     if (res.status === 429) {
-        rateLimitedCount.add(1); // Gateway chặn thành công
+        rateLimitedCount.add(1);
     } else if (res.status === 200) {
-        successCount.add(1);     // Login thành công (chưa bị chặn)
+        successCount.add(1);
     } else {
-        // Log ra nếu gặp lỗi lạ (ví dụ 401 do sai pass, 500 lỗi server)
+
         console.log(`Status khác: ${res.status}`);
     }
 }
